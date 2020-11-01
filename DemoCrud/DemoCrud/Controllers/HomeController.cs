97cf32fe8bcb87c39c21621b2c7ef249419e8a9c
nihelhuang <nihelhuang@gmail.com>
2020-11-01 23:00:33 +0800

    New Action

--- DemoCrud/DemoCrud/Controllers/HomeController.cs
+++ DemoCrud/DemoCrud/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoCrud.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,59 @@ namespace DemoCrud.Controllers
 {
     public class HomeController : Controller
     {
+        private UserDataService service;
+
+        public HomeController()
+        {
+            service = new UserDataService();
+        }
+
         public ActionResult Index()
         {
+            var result = service.GetAllData();
+
+            return View(result);
+        }
+
+        public ActionResult Insert()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Insert(string name, int age, string city)
+        {
+            service.CreateUserData(new UserDataModel()
+            {
+                name = name,
+                age = age,
+                city = city
+            });
+
+            return Json(new { isSuccess = true }); ;
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string pk)
+        {
+            service.DeleteUserData(pk);
+
+            return Json(new { isSuccess = true }); ;
+        }
+
+        public ActionResult Update(string pk)
+        {
+            var result = service.GetDataByPK(pk);
+
+            return View(result);
+        }
+
+        [HttpPost]
+        public ActionResult Update(UserDataModel user)
+        {
+            service.UpdateUserData(user);
+
+            return Json(new { isSuccess = true }); ;
         }
 
         public ActionResult About()

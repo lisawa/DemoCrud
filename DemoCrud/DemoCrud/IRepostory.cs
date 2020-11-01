cee87202e01bc482619a034c9307e5ef73ac78fb
nihelhuang <nihelhuang@gmail.com>
2020-11-01 16:11:30 +0800

    Set repository layer

---
+++ DemoCrud/DemoCrud/IRepostory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Demo
+{
+    public interface IRepository<T>
+    {
+        void Create(T entity);
+
+        T Read(Expression<Func<T, bool>> predicate);
+
+        IQueryable<T> Reads();
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        void SaveChanges();
+    }
+}
---
+++ DemoCrud/DemoCrud/Repository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Demo
+{
+    public class Repository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        private DbContext Context { get; set; }
+
+        public Repository(DbContext inContext)
+        {
+            Context = inContext;
+        }
+
+        public void Create(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+        }
+
+        public TEntity Read(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().Where(predicate).FirstOrDefault();
+        }
+
+        public IQueryable<TEntity> Reads()
+        {
+            return Context.Set<TEntity>().AsQueryable();
+        }
+
+        public void Update(TEntity entity)
+        {
+            Context.Entry<TEntity>(entity).State = EntityState.Modified;
+        }
+
+        public void Update(TEntity entity, Expression<Func<TEntity, object>>[] updateProperties)
+        {
+            Context.Configuration.ValidateOnSaveEnabled = false;
+
+            Context.Entry<TEntity>(entity).State = EntityState.Unchanged;
+
+            if (updateProperties != null)
+            {
+                foreach (var property in updateProperties)
+                {
+                    Context.Entry<TEntity>(entity).Property(property).IsModified = true;
+                }
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            Context.Entry<TEntity>(entity).State = EntityState.Deleted;
+        }
+
+        public void SaveChanges()
+        {
+            Context.SaveChanges();
+
+            if (Context.Configuration.ValidateOnSaveEnabled == false)
+            {
+                Context.Configuration.ValidateOnSaveEnabled = true;
+            }
+        }
+    }
+
+}
